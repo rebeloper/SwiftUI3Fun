e767e090b32666b35c8cedcfc8de17b428ded282
Alex Nagy <therebeloper@gmail.com>
2021-06-10 21:13:08 +0300

    Initial Commit

---
+++ SwiftUI3Fun/SwiftUI3FunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3FunApp.swift
+//  SwiftUI3Fun
+//
+//  Created by Alex Nagy on 10.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3FunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
